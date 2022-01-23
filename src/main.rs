9f015aeb36c30953ddaeec57851203d2429c1147
fack <>
2022-01-22 23:39:31 -0600

    gtfs work

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use clap::{AppSettings, Parser};
 use tokio::select;
 use gtfs_structures::Gtfs;
 
+const GTFS_ZIP: &str = "https://svc.metrotransit.org/mtgtfs/gtfs.zip";
+
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 #[clap(setting(AppSettings::ArgRequiredElseHelp))]
@@ -43,9 +45,11 @@ struct Cli {
 //starting small, display bus info in terminal
 fn main() {
     let cli = Cli::parse();
-    let gtfs = gtfs_structures::Gtfs::new("https://svc.metrotransit.org/mtgtfs/gtfs.zip");
-
+    
     if cli.verbose == true {
+        println!("Printing stats:");
+        let gtfs = gtfs_structures::Gtfs::new(GTFS_ZIP).expect("error reading zip from url");
+        gtfs.print_stats();
         println!("verbosity is enabled, you will now see all println! debugging");
         if cli.alert == true {
             println!("Alerting is enabled, maybe a beep will happen or something");
