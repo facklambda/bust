4ab07db091789dbf69f7e2200322b405453fde81
fack <fack>
2022-02-13 02:29:38 -0600

    direction is working right now, as is gtfs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use clap::{AppSettings, Parser};
 use gtfs_structures::StopTime;
 use tokio::select;
 
-const GTFS_ZIP: &str = "./gtfs/"; //"https://svc.metrotransit.org/mtgtfs/gtfs.zip";
+//const GTFS_ZIP: &str = "./gtfs/";
+const GTFS_ZIP: &str = "https://svc.metrotransit.org/mtgtfs/gtfs.zip";
+
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -18,7 +20,7 @@ struct Cli {
 
     /// Direction of route
     #[clap(short, long, arg_enum)]
-    direction: Direction,
+    direction: Option<Direction>,
 
     /// Force usage of Nextrip API instead of local GTFS archive
     #[clap(long)]
@@ -35,23 +37,25 @@ struct Cli {
 
 #[derive(clap::ArgEnum, Clone, Debug)]
 enum Direction {
-   north,
-   south,
-   east,
-   west,
+   North,
+   South,
+   East,
+   West,
 }
 
 //starting small, display bus info in terminal
 fn main() {
     let cli = Cli::parse();
-    // let gtfs = gtfs_structures::GtfsReader::default()
-    //     .read_stop_times(false)
-    //     .read(GTFS_ZIP)
-    //     .expect("impossible to read gtfs");
-    // gtfs.print_stats();
+    let gtfs = gtfs_structures::GtfsReader::default()
+        .read_stop_times(false)
+        .read(GTFS_ZIP)
+        .expect("impossible to read gtfs");
+    gtfs.print_stats();
 
     println!("Fetching timetable for the stop: {:?}", cli.stop_id);
     println!("Fetching timetable for route: {:?}, serving stop: {:?}", cli.route_id, cli.stop_id);
     println!("Fetching timetable for {:?}bound route: {:?}, serving stop: {:?}", cli.direction, cli.route_id, cli.stop_id);
     println!("Fetching timetable for all {:?}bound routes serving stop: {:?}", cli.direction, cli.stop_id);
+
+    println!("stop data: {:?}", gtfs.get_stop(&cli.stop_id));
 }
