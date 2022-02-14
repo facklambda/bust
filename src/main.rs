2f14c5ea734a7edd6b38c9ebb6817368b6c5eae9
fack <fack>
2022-02-13 22:33:36 -0600

    stubbing out nextrip flag

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use tokio::select;
 //const GTFS_ZIP: &str = "./gtfs/";
 const GTFS_ZIP: &str = "https://svc.metrotransit.org/mtgtfs/gtfs.zip";
 
-
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 #[clap(setting(AppSettings::ArgRequiredElseHelp))]
@@ -32,30 +31,49 @@ struct Cli {
     /// Format output to JSON
     #[clap(long)]
     json: bool,
-
 }
 
 #[derive(clap::ArgEnum, Clone, Debug)]
 enum Direction {
-   North,
-   South,
-   East,
-   West,
+    North,
+    South,
+    East,
+    West,
 }
 
 //starting small, display bus info in terminal
 fn main() {
     let cli = Cli::parse();
-    let gtfs = gtfs_structures::GtfsReader::default()
-        .read_stop_times(false)
-        .read(GTFS_ZIP)
-        .expect("impossible to read gtfs");
-    gtfs.print_stats();
 
-    println!("Fetching timetable for the stop: {:?}", cli.stop_id);
-    println!("Fetching timetable for route: {:?}, serving stop: {:?}", cli.route_id, cli.stop_id);
-    println!("Fetching timetable for {:?}bound route: {:?}, serving stop: {:?}", cli.direction, cli.route_id, cli.stop_id);
-    println!("Fetching timetable for all {:?}bound routes serving stop: {:?}", cli.direction, cli.stop_id);
+    if cli.nextrip {
+        println!("local GTFS ignored, requesting data from NexTrip API");
+        
+    }
 
-    println!("stop data: {:?}", gtfs.get_stop(&cli.stop_id));
+    if cli.route_id.is_some() && cli.direction.is_none() {
+        println!(
+            "Fetching timetable for route: {:?}, serving stop: {:?}",
+            cli.route_id, cli.stop_id
+        );
+    } else if cli.direction.is_some() && cli.route_id.is_some() {
+        println!(
+            "Fetching timetable for {:?}bound route: {:?}, serving stop: {:?}",
+            cli.direction, cli.route_id, cli.stop_id
+        );
+    } else if cli.direction.is_some() && cli.route_id.is_none() {
+        println!(
+            "Fetching timetable for all {:?}bound routes serving stop: {:?}",
+            cli.direction, cli.stop_id
+        );
+    } else {
+        println!("Fetching timetable for the stop: {:?}", cli.stop_id);
+    }
+    // commenting this out because it makes the test runs take like 30 seconds (fetching the gtfs zip, unzipping, and then parsing seems to be quite slow)
+    // let gtfs = gtfs_structures::GtfsReader::default()
+    //     .read_stop_times(false)
+    //     .read(GTFS_ZIP)
+    //     .expect("impossible to read gtfs");
+    // gtfs.print_stats();
+
+    // println!("stop data: {:?}", gtfs.get_stop(&cli.stop_id));
 }
